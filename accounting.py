ba9fe4101e00381bf1b06a4e5bf6f2be2b91facb
vagrant <vagrant@vagrant.vm>
2020-10-02 07:05:01 +0000

    changed melon_bought and amount_paid to float

--- accounting.py
+++ accounting.py
@@ -5,24 +5,30 @@ def accounting(path):
     melon_cost = 1.00
     order_log = open(path)
 
-    #split the report to parts
+    #iterate over each 
     for line in order_log:
         line = line.rstrip()
+        #split line where "|" is to get the info
         info = line.split("|")
 
-        #create variables to work with
-        customer = info[1]
-        melons_bought = info[2]
-        amount_paid = info[3]
-    
-        #see how much the sale should have costed
-        correct_sale = melon_cost * int(melons_bought)
-        
-        #checks if the sale is correct, and if the sale does not match the actual cost:
-        #print statment
+        #get info and save it to variables
 
-        if float(correct_sale) != float(amount_paid):
-            print(f"{customer} paid {float(amount_paid):.2f}, expected {float(correct_sale):.2f}.")
+        #customer name
+        customer = info[1]
+        
+        #how many melons they bought and how much they paid
+        melons_bought = float(info[2])
+        amount_paid = float(info[3])
+    
+        #calculate how much the sale should have costed
+        correct_sale = melon_cost * melons_bought
+        
+        #checks if the sale in the report is correct, and if not print how much they paid and how much was expected
+
+        if correct_sale != amount_paid:
+            print(f"{customer} paid {amount_paid:.2f}, expected {correct_sale:.2f}.")
+
+        #if correct_sale < 
 
 
     order_log.close()
