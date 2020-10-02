ca1edbf1a10a3b02f619d78f8caa71d68a60de0c
vagrant <vagrant@vagrant.vm>
2020-10-02 06:55:56 +0000

    added variables, correct sale, and string output

--- accounting.py
+++ accounting.py
@@ -9,14 +9,25 @@ def accounting(path):
     for line in order_log:
         line = line.rstrip()
         info = line.split("|")
-        print(info)
+
+        #create variables to work with
+        customer = info[1]
+        melons_bought = info[2]
+        amount_paid = info[3]
+    
+        #see how much the sale should have costed
+        correct_sale = melon_cost * int(melons_bought)
+        
+        #checks if the sale is correct, and if the sale does not match the actual cost:
+        #print statment
+
+        if float(correct_sale) != float(amount_paid):
+            print(f"{customer} paid {float(amount_paid):.2f}, expected {float(correct_sale):.2f}.")
+
 
     order_log.close()
     
 accounting("customer-orders.txt")
-    #create variables to work with
-    #see how much the sale should have costed
-    #see how much the customer paid
-    #if the sale does not match the actual cost:
-        #print statment
+    
+    
 
